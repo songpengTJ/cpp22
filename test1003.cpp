2d4b72a61cca1a61d86fde9e224fdc04aeb50dc4
SongPeng <songpeng@blwl.net>
2022-10-07 22:24:25 +0800

    1

--- test1003.cpp
+++ test1003.cpp
@@ -1,6 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
-int main()
+#define LENGTH  6
+
+int main0()
 {
     const char *s ={"87.99%已完成"};
     char *p;
@@ -11,7 +13,23 @@ int main()
     printf("%.2f后的字符串是：%s\n",d,p);
 
     long l = 1234567890;
-    printf("数值是: %ld\n",l);
+    printf("数值是: %ld\n\n",l);
     
     return 0;
 }
+ 
+ int main()
+ {
+
+    main0();
+
+    double d =123.45678;
+    char p[LENGTH+2];
+    gcvt(d,LENGTH,p);
+    puts(p);
+    char q[100];
+    sprintf(q,"%4.2f%s\n",d,"已转换");
+    puts(q);
+
+    return 0;
+ }
